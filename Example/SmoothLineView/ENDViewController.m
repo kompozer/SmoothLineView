eceb508b7d133c75f9d2ad6750147ba50336a441
Andreas Kompanez <akm@mm.st>
2015-05-31 14:32:36 +0200

    Filling the draw canvas with color on long press

--- Example/SmoothLineView/ENDViewController.m
+++ Example/SmoothLineView/ENDViewController.m
@@ -12,7 +12,7 @@
 #import <PureLayout/PureLayout.h>
 #import <CoreMotion/CoreMotion.h>
 
-@interface ENDViewController ()
+@interface ENDViewController () <LVSmoothLineViewDelegate>
 
 @property (nonatomic, strong) LVSmoothLineView *smoothLineView;
 
@@ -26,6 +26,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.smoothLineView = [[LVSmoothLineView alloc] initForAutoLayout];
+    self.smoothLineView.delegate = self;
     [self.view addSubview:self.smoothLineView];
     [self.smoothLineView autoPinEdgesToSuperviewEdgesWithInsets:(ALEdgeInsets){0, 0, 0, 0}];
 }
@@ -39,6 +40,11 @@
 -(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
 {
     [self.smoothLineView undo];
+}
+
+- (void)smoothLineViewLongPressed:(LVSmoothLineView *)view
+{
+    [self.smoothLineView fillWithColor:self.smoothLineView.lineColor];
 }
 
 
